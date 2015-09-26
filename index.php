b96fb211dc41f4090672b4fb7056775c2f16d735
Thomas Elvin <thom855j@cvkweb.dk>
2015-09-26 16:34:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Eksamensprojekt 2015: Happy House 
+ * PHP MVC CMF
  *
  * @author thom855j, with inspiration form panique/mini and CodeCourse
  * @license http://opensource.org/licenses/MIT MIT License
