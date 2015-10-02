b814831f2d9e8c54d35d78581ca32dc83db51ac2
Thomas Elvin <sde.thom855j@gmail.com>
2015-10-02 23:52:12 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,5 +5,33 @@
  * @author thom855j, with inspiration form panique/mini and CodeCourse
  * @license http://opensource.org/licenses/MIT MIT License
  */
+// Get config
+require_once '../app/config.php';
+// Start Composers autoloader
+require_once '../vendor/autoload.php';
+
+// Start bootstrap of app
+require_once '../app/bootstrap.php';
+use WebSupportDK\PHPMvcFramework\Router;
+
+$params = array(
+	'controller' => 'page',
+	'action' => 'name',
+	'path_controller' => PATH_APP_CONTROLLERS,
+	'root_url' => 'url'
+);
 
 
+$app = new Router($params);
+
+use WebSupportDK\PHPHttp\Url;
+
+switch (Url::getError()) {
+	case 404:
+		Url::redirect(Url::getRoot('public'). 'error/code/404');
+
+		break;
+
+	default:
+		break;
+}
