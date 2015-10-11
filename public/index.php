cdb94910267eeeaadfb577bb125ebe4723cdc99e
Thomas Elvin <sde.thom855j@gmail.com>
2015-10-11 14:40:36 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
  * @author thom855j, with inspiration form panique/mini and CodeCourse
  * @license http://opensource.org/licenses/MIT MIT License
  */
-<<<<<<< HEAD
+
 ini_set('display_errors', true);
 // Start bootstrap of app
 require_once '../app/bootstrap.php';
@@ -40,35 +40,3 @@ switch ($app->get('mode')) {
 
 
 require_once 'errors.php';
-=======
-// Get config
-require_once '../app/config.php';
-// Start Composers autoloader
-require_once '../vendor/autoload.php';
-
-// Start bootstrap of app
-require_once '../app/bootstrap.php';
-use WebSupportDK\PHPMvcFramework\Router;
->>>>>>> origin/master
-
-$params = array(
-	'controller' => 'page',
-	'action' => 'name',
-	'path_controller' => PATH_APP_CONTROLLERS,
-	'root_url' => 'url'
-);
-
-
-$app = new Router($params);
-
-use WebSupportDK\PHPHttp\Url;
-
-switch (Url::getError()) {
-	case 404:
-		Url::redirect(Url::getRoot('public'). 'error/code/404');
-
-		break;
-
-	default:
-		break;
-}
