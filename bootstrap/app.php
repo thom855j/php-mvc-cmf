f31ad8c6341306e6afbc626f59543db605dfbb64
Thomas Elvin <thom855j@cvkweb.dk>
2015-10-16 21:46:42 +0200

    email fix
    fix for email

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -191,13 +191,13 @@ $app->set('Mailer', new $driver);
 $app->get('Mailer')->isSMTP();
 $app->get('Mailer')->CharSet = $app->get('config.app.charset');
 $app->get('Mailer')->Host = $app->get('config.mail.host');
-$app->get('Mailer')->SMTPAuth = true;
+$app->get('Mailer')->SMTPAuth = TRUE;
 $app->get('Mailer')->SMTPSecure = $app->get('config.mail.encryption');
 $app->get('Mailer')->Port = $app->get('config.mail.port');
 $app->get('Mailer')->Username = $app->get('config.mail.username');
 $app->get('Mailer')->Password = $app->get('config.mail.password');
 $app->get('Mailer')->setFrom($app->get('config.mail.username'), $app->get('config.app.name'));
-$app->get('Mailer')->isHTML( true );
+$app->get('Mailer')->isHTML( TRUE );
 		// Set debug
 		if($app->get('config.app.debug')){
 		$app->get('Mailer')->SMTPDebug = TRUE;
