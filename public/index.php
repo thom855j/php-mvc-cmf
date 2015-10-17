1554120eabc640b8c814017d3ec296a9844d1f20
Thomas Elvin <thom855j@cvkweb.dk>
2015-10-17 02:58:05 +0200

    rename
    rename

--- public/index.php
+++ public/index.php
@@ -24,7 +24,7 @@ require __DIR__ . '/../bootstrap/autoload.php';
   | Turn On The Lights
   |--------------------------------------------------------------------------
   |
-  | We need to illuminate PHP development, so let us turn on the lights.
+  | We need to start the PHP development, so let us turn on the lights.
   | This bootstraps the framework and gets it ready for use, then it
   | will load up this application so that we can run it and send
   | the responses back to the browser and delight our users.
